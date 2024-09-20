9e1d42593f1029cc61047e8d4ac125d146d5d28a
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-20 17:03:19 +0300

    Solve 004 Problem

---
+++ Arrays-Strings/004_reverse_words_of_string.py
@@ -0,0 +1,17 @@
+def reverse_words(s: str) -> str:
+    words = []
+    temp = ""
+    for c in s:
+        if c != " ":
+            temp += c
+        elif temp != "":
+            words.append(temp)
+            temp = ""
+    if temp != "":
+        words.append(temp)
+    return " ".join(words[::-1])
+
+# Test cases
+print(reverse_words("the sky is blue"))  # "blue is sky the"
+print(reverse_words("  hello world  "))  # "world hello"
+print(reverse_words("a good   example"))  # "example good a"
