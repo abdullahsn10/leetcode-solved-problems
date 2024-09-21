efffab9a65ab6ce8e294a8812877b6799adf6dad
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-21 17:09:40 +0300

    Add BFS solution for max Depth

--- Tree/005_max_depth_of_BT.py
+++ Tree/005_max_depth_of_BT.py
@@ -13,3 +13,21 @@ def max_depth(root: Optional[TreeNode]) -> int:
         return 0
 
     return 1 + max(max_depth(root.left), max_depth(root.right))
+
+
+def max_depth_BFS(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+
+    queue = []
+    queue.append(root)
+    levels = 0
+    while queue:
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        levels += 1
+    return levels
